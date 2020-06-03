2de28dcd6a7a40495502565ddd0d995823301849
TeodorDyakov <tedy1996@gmail.com>
2020-06-03 21:12:14 +0300

    changed the algorithm to BFS search. Now it should be much faster for almost all cases.

--- Rubiks.java
+++ Rubiks.java
@@ -1,9 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Rubiks {
 
@@ -14,11 +9,7 @@ public class Rubiks {
 	static final int F = 4;
 	static final int Fprim = 5;
 
-	static final int[] reverseMoves = new int[] { Rprim, R, Uprim, U, Fprim, F };
 	static final String moves[] = new String[] { "R", "R'", "U", "U'", "F", "F'" };
-
-	static int cube[] = new int[25];
-	static int cubeNew[] = new int[25];
 
 	static int[] convertScrambleToCubeArray(String scramble) {
 		int[] cube = new int[25];
@@ -62,42 +53,99 @@ public class Rubiks {
 		return true;
 	}
 
-	static boolean isTopOriented(int[] cube) {
-		return cube[1] == cube[2] && cube[3] == cube[2] && cube[4] == cube[3];
+	static class CubeState{
+		int move;
+		int[] arr;
+
+		CubeState(int[] arr, int move){
+			this.arr = arr;
+			this.move = move;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			CubeState cubeState = (CubeState) o;
+			return Arrays.equals(arr, cubeState.arr);
+		}
+
+		@Override
+		public int hashCode() {
+			return Arrays.hashCode(arr);
+		}
 	}
 
-	static boolean isLayerOneSolved(int[] cube) {
-		return cube[7] == cube[8] && cube[9] == cube[10] && cube[10] == cube[11] && cube[11] == cube[12]
-				&& cube[15] == cube[16] && cube[19] == cube[20] && cube[23] == cube[24];
+	static void traceSteps(CubeState cs, Map<CubeState, CubeState>graph){
+		List<String>steps = new ArrayList<>();
+		while(cs != null) {
+			steps.add(moves[cs.move]);
+			cs = graph.get(cs);
+		}
+		steps.remove(steps.size()-1);
+		Collections.reverse(steps);
+		System.out.println(steps);
 	}
 
-	static void rotate(int side) {
+	static void bfs(CubeState cubeState){
+		Map<CubeState, CubeState>prev = new HashMap();
+		Queue<CubeState> q = new ArrayDeque<>();
+		q.add(cubeState);
+		prev.put(cubeState, null);
+
+		while(!q.isEmpty()) {
+			CubeState curr = q.poll();
+			if(checkSolved(curr.arr)){
+				traceSteps(curr, prev);
+				break;
+			}
+			for (CubeState neigh : neighbours(curr)) {
+				if (!prev.containsKey(neigh)) {
+					prev.put(neigh, curr);
+					q.add(neigh);
+				}
+			}
+		}
+	}
+
+	static List<CubeState>neighbours(CubeState cubeState){
+		List<CubeState>neigh = new ArrayList<>();
+		for(int i = 0; i < 6; i++){
+			int[] stateArr = rotate(i, cubeState.arr);
+			neigh.add(new CubeState(stateArr, i));
+		}
+		return neigh;
+	}
+
+	static int[]rotate(int side, int []cube) {
+		int cubeNew[] = new int[25];
 		for (int i = 1; i < cube.length; i++) {
-			cube[i] = cubeNew[i];
+			cubeNew[i] = cube[i];
 		}
 		switch (side) {
 		case U:
-			rotateU();
+			rotateU(cubeNew, cube);
 			break;
 		case Uprim:
-			rotateUprim();
+			rotateUprim(cubeNew, cube);
 			break;
 		case R:
-			rotateR();
+			rotateR(cubeNew, cube);
 			break;
 		case Rprim:
-			rotateRprim();
+			rotateRprim(cubeNew, cube);
 			break;
 		case F:
-			rotateF();
+			rotateF(cubeNew, cube);
 			break;
 		case Fprim:
-			rotateFprim();
+			rotateFprim(cubeNew, cube);
 			break;
 		}
+		return cubeNew;
 	}
 
-	static void rotateU() {
+	static void rotateU(int[] cubeNew, int[] cube) {
 		cubeNew[5] = cube[21];
 		cubeNew[6] = cube[22];
 		cubeNew[1] = cube[3];
@@ -112,7 +160,7 @@ public class Rubiks {
 		cubeNew[4] = cube[2];
 	}
 
-	static void rotateUprim() {
+	static void rotateUprim(int[] cubeNew, int[] cube) {
 		cubeNew[1] = cube[2];
 		cubeNew[2] = cube[4];
 		cubeNew[4] = cube[3];
@@ -127,7 +175,7 @@ public class Rubiks {
 		cubeNew[18] = cube[14];
 	}
 
-	static void rotateR() {
+	static void rotateR(int[] cubeNew, int[] cube) {
 		cubeNew[2] = cube[6];
 		cubeNew[4] = cube[8];
 		cubeNew[21] = cube[23];
@@ -142,7 +190,7 @@ public class Rubiks {
 		cubeNew[15] = cube[2];
 	}
 
-	static void rotateRprim() {
+	static void rotateRprim(int[] cubeNew, int[] cube) {
 		cubeNew[2] = cube[15];
 		cubeNew[4] = cube[13];
 		cubeNew[6] = cube[2];
@@ -157,7 +205,7 @@ public class Rubiks {
 		cubeNew[22] = cube[24];
 	}
 
-	static void rotateF() {
+	static void rotateF(int[] cubeNew, int[] cube) {
 		cubeNew[3] = cube[20];
 		cubeNew[4] = cube[18];
 		cubeNew[21] = cube[3];
@@ -172,7 +220,7 @@ public class Rubiks {
 		cubeNew[8] = cube[6];
 	}
 
-	static void rotateFprim() {
+	static void rotateFprim(int[] cubeNew, int[] cube) {
 		cubeNew[20] = cube[3];
 		cubeNew[18] = cube[4];
 		cubeNew[3] = cube[21];
@@ -187,66 +235,6 @@ public class Rubiks {
 		cubeNew[6] = cube[8];
 	}
 
-	static String algoToString(int[] algo) {
-		String s = "";
-		for (int i : algo) {
-			s += moves[i] + " ";
-		}
-		return s;
-	}
-
-	static void execAlg(int[] alg) {
-		for (int move : alg) {
-			rotate(move);
-		}
-	}
-
-	static Map<String, Integer> dp = new HashMap<>();
-
-	// ogbyyrggwwwwybbbyrrrgooo
-	static void solve(String algo, int numberOfMoves, int prevMove) {
-		if (numberOfMoves >= 12) {
-			return;
-		}
-
-		if (checkSolved(cubeNew)) {
-			solves.add(algo);
-			return;
-		}
-		String cubeState = Arrays.toString(cubeNew);
-		Integer movesToState = dp.get(cubeState);
-
-		if (movesToState == null || movesToState > numberOfMoves) {
-			dp.put(cubeState, numberOfMoves);
-		} else if (movesToState <= numberOfMoves) {
-			return;
-		}
-
-		for (int move = 0; move < 6; move++) {
-			if (reverseMoves[move] == prevMove) {
-				continue;
-			}
-			rotate(move);
-			solve(algo + moves[move], numberOfMoves + 1, move);
-			rotate(reverseMoves[move]);
-		}
-	}
-
-	static List<String> solves = new ArrayList<>();
-
-	static String shortestSolve() {
-		int minLen = 20;
-		String min = "";
-		// System.out.println(solves);
-		for (int i = 0; i < solves.size(); i++) {
-			if (solves.get(i).length() < minLen) {
-				minLen = solves.get(i).length();
-				min = solves.get(i);
-			}
-		}
-		return min;
-	}
-
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter cube scramble:\n ");
@@ -258,13 +246,13 @@ public class Rubiks {
 		System.out.println("o-orange");
 
 		String scramble = in.next();
-		int[] cubeInitialState = convertScrambleToCubeArray(scramble);
-		cubeNew = Arrays.copyOf(cubeInitialState, cubeNew.length);
+		int[] cube = convertScrambleToCubeArray(scramble);
 
 		long tic = System.currentTimeMillis();
-		solve("", 0, 7);
-		System.out.printf("Time taken to find shorest solution, %fs\n%s",
-				(System.currentTimeMillis() - tic) / 1000f, shortestSolve());
+		CubeState init = new CubeState(cube, 0);
+		bfs(init);
+		System.out.printf("Time taken to find shorest solution, %fs\n",
+				(System.currentTimeMillis() - tic) / 1000f);
 
 	}
 }
